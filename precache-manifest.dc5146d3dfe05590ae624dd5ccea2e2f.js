d3de56f4ba301c4600294631be5b0f1b4a46c1e5
Sumukh Lagadamane Shivashankara <ls.sumukh@gmail.com>
2021-02-24 17:35:38 -0700

    try2

---
+++ precache-manifest.dc5146d3dfe05590ae624dd5ccea2e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d05f8593a5bee54ae6e5b41785a6b9",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "bd786e9090452448739a",
+    "url": "/weather-app-react/static/css/main.734dc181.chunk.css"
+  },
+  {
+    "revision": "543fae73a1761cac0626",
+    "url": "/weather-app-react/static/js/2.3a396725.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/weather-app-react/static/js/2.3a396725.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd786e9090452448739a",
+    "url": "/weather-app-react/static/js/main.ac15d3d7.chunk.js"
+  },
+  {
+    "revision": "d9885b805ae5faf1bd40",
+    "url": "/weather-app-react/static/js/runtime-main.cdab807e.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/weather-app-react/static/media/2850815.246c1201.jpg"
+  }
+]);
